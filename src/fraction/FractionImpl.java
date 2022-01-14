44106087c48d0e7c55011bce141078c0062a1e11
Ana Castro <analuiza_j@hotmail.com>
2022-01-14 00:11:28 +0000

    I implemented method compareTo of class FractionImpl.

--- src/fraction/FractionImpl.java
+++ src/fraction/FractionImpl.java
@@ -208,15 +208,33 @@ public class FractionImpl implements Fraction {
     }
 
     /**
-     * @inheritDoc
+     * Returns:
+     * <ul>
+     *     <li>A negative <pre>int</pre> if <pre>this</pre> is less than <pre>o</pre>.</li>
+     *     <li>Zero if <pre>this</pre> is equal to <pre>o</pre>.</li>
+     *     <li>A positive <pre>int</pre> if <pre>this</pre> is greater than <pre>o</pre>.</li>
+     * </ul>
+     *
+     * @param f the fraction to compare <pre>this</pre> to
+     * @return the result of the comparison
      */
     @Override
-    public int compareTo(Fraction o) {
-        return 0;
+    public int compareTo(Fraction f) {
+        FractionImpl fCopy = new FractionImpl(f.toString());
+        if (numerator / denominator > fCopy.numerator / fCopy.denominator) return 1;
+        else if (numerator / denominator < fCopy.numerator / fCopy.denominator) return -1;
+        else return 0;
     }
 
     /**
-     * @inheritDoc
+     * Returns a <pre>String</pre> of the form <pre>n/d</pre>, where <pre>n</pre> is the
+     * <em>numerator</em> and <pre>d</pre> is the <em>denominator</em>.
+     * However, if <pre>d</pre> is <pre>1</pre>, just return <pre>n</pre> (as a <pre>String</pre>).
+     * <p>
+     * The returned <pre>String</pre> should not contain any blanks.
+     * If the fraction represents a negative number, a minus sign should precede <pre>n</pre>
+     *
+     * @return the string representation fo the fraction
      */
     @Override
     public String toString() {
